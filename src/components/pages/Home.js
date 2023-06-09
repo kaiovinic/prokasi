007b4475f4b64cafb5e1da5db35d6b22702d1b7c
kaiovinic <kaiovini19@hotmail.com>
2023-06-09 01:31:57 -0300

    Criação da Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
+import styles from "./Home.module.css";
+import savings from "../../img/savings.svg";
+import LinkBuntton from "../layout/LinkButton";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.home}>
+      <h1>
+        bem-vindo! ao <span>Prokasi</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkBuntton to="/newproject" text="Criar Projeto" />
+      <img src={savings} alt="Prokasi" />
+    </section>
+  );
 }
 
 export default Home;
